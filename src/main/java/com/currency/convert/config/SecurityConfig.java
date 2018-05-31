5fd6ffef80002651da551208633536bd94ef09d6
Yeshendra Verma <yeshendra.verma@freschesolutions.com>
2018-05-31 17:32:42 +0530

    working changes

--- src/main/java/com/currency/convert/config/SecurityConfig.java
+++ src/main/java/com/currency/convert/config/SecurityConfig.java
@@ -5,11 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.currency.convert.service.impls.UserDetailsServiceImpl;
 
@@ -29,11 +30,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/", "/hello").authenticated().and().formLogin().loginPage("/login").and()
-				.logout().permitAll();
-
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.csrf().requireCsrfProtectionMatcher(new AntPathRequestMatcher("**/login")).and().authorizeRequests()
+				.antMatchers("/hello").authenticated().and().formLogin().defaultSuccessUrl("/hello").loginPage("/login")
+				.and().logout().permitAll();
 
 	}
 
@@ -46,6 +45,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public AuthenticationManager authenticationManager() throws Exception {
 		return super.authenticationManager();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/*.css");
+		web.ignoring().antMatchers("/*.js");
 	}
 
 }
--- src/main/java/com/currency/convert/controller/HomeController.java
+++ src/main/java/com/currency/convert/controller/HomeController.java
@@ -2,17 +2,17 @@ package com.currency.convert.controller;
 
 import java.security.Principal;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
 	@GetMapping("/hello")
 	public String hello() {
-		return "/hello";
+		return "hello";
 	}
 
 	@PostMapping("/hello")
